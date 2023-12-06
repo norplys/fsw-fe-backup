44f73906f0d9dffe76a983ca6151ca20ef223d26
norplys <joywinata88@gmail.com>
2023-12-06 10:46:24 +0700

    feat : updating page detail, adding iframe

--- components/ButtonLoading.jsx
+++ components/ButtonLoading.jsx
@@ -1,6 +1,6 @@
 export default function ButtonLoading() {
     return (
-    <div className="animate-pulse grid gap-3 h-6 flex-1 rounded-xl shadow-2xl mb-4">
+    <div className="animate-pulse grid gap-3 h-6 flex-1 rounded-xl shadow-2xlmb-4">
         <div className=" row-span-4 bg-secret-cyan rounded-3xl"></div>
     </div>
     )
