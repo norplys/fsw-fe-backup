7b02ba4ad9f1269e1930ca3cbf67fbbb0e781fec
norplys <joywinata88@gmail.com>
2023-12-07 23:57:28 +0700

    fix : fixing conflict

--- app/(auth)/login/forgot-password/page.jsx
+++ app/(auth)/login/forgot-password/page.jsx
@@ -30,7 +30,7 @@ export default function ForgotPassword() {
           <div className="text-secret-text items-center text-center mt-6">
             Belum punya akun?
             <Link
-              href="/"
+              href="/register"
               className="text-secret-text font-bold hover:underline hover:text-white pl-2"
             >
               Daftar di sini
