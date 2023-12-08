a6bacfbce1586af5b642c3ad8749233b5e0e2f4a
norplys <joywinata88@gmail.com>
2023-12-08 10:46:44 +0700

    fix : fixing class button and my-class

---
+++ components/ClassPage/MyClassCard.jsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import Link from "next/link";
+import { GiRank3 } from "react-icons/gi";
+import { FaBookBookmark, FaRegClock } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa";
+
+export default function MyClassCard({
+  name,
+  image,
+  rating,
+  teacher,
+  category,
+  level,
+  totalModule,
+  totalMinute,
+  id,
+  totalProgress
+}) {
+  totalProgress = "50%"
+  return (
+    <Link
+      href={`course/${id}`}
+      className="flex flex-col course w-full h-[250px] bg-secret-background rounded-[15px] duration-300 shadow-xl hover:scale-105 hover:shadow-2xl"
+    >
+      <div className="w-full h-[80px] ">
+        <Image
+          src={image}
+          width={323}
+          height={80}
+          className="w-full h-full object-cover rounded-t-2xl"
+          alt="gambar konten"
+        />
+      </div>
+
+      <div className="flex flex-col justify-around p-2 h-full">
+        <div className="flex justify-between">
+          <p className="text-lg font-bold text-secret-pink">{category}</p>
+
+          <div className="flex gap-1 text-black items-center">
+            <FaStar className="text-yellow-400 text-lg" />
+            <p className="text-secret-text3 font-semibold text-sm">{rating}</p>
+          </div>
+        </div>
+
+        <div className="text-black">
+          <h2 className="text-base font-bold">{name}</h2>
+
+          <p className="text-sm">by {teacher}</p>
+        </div>
+
+        <div className="flex justify-between mr-[10px] mt-1">
+          <div className="flex gap-1 leading-loose">
+            <GiRank3 className="text-green-700 text-lg"/>
+            <p className="font-semibold text-xs text-secret-text3">
+              {level} Level
+            </p>
+          </div>
+          <div className="flex gap-1 leading-loose">
+          <FaBookBookmark className="text-green-700 text-md"/>
+            <p className="text-xs font-semibold text-secret-text3">{totalModule} Modul</p>
+          </div>
+          <div className="flex gap-1 leading-loose">
+          <FaRegClock className="text-green-700  text-md"/>
+            <p className="text-xs font-semibold text-secret-text3">{totalMinute} Menit</p>
+          </div>
+        </div>
+        <div className="w-[150px] bg-secret-pink rounded-[25px]">
+            <div className="bg-secret-darkblue rounded-[25px] py-1" style={{width : totalProgress}}>
+            <p className="text-xs font-bold text-secret-background pl-3">{totalProgress}</p>
+            </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
