a1c90fc27279e8d76d8890fe382dde9b1a66e49e
norplys <joywinata88@gmail.com>
2023-12-10 20:55:28 +0700

    feat : update branch

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -3,12 +3,57 @@
 
 import { BiBrain } from "react-icons/bi";
 import Link from "next/link";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useUsers } from "@/app/context/usersContext";
+import { useState } from "react";
 
 export default function LoginPage() {
+  const {users, handleUsers} = useUsers();
+  const {push} = useRouter();
+  const [disabled, setDisabled] = useState(false);
+  
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    // setDisabled(true);
+    const formData = new FormData(e.currentTarget);
+    const formObject = Object.fromEntries(formData);
+    const mock  =  mockLogin();
+    console.log(formObject);
+    handleUsers(formObject);
+    const res = await toast.promise (
+      mock , {
+        loading: 'Loading',
+        success: `${formObject.Email} successfully logged in`,
+        error: 'Error'
+      }
+    )
+    console.log(res);
+    // await sleepRedirect();
+
+  }
+
+  const sleepRedirect = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(push('/'));
+      }, 1000);
+    });
+  }
+
+  const mockLogin = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(true);
+      }, 1000);
+    });
+}
+
   return (
     <div className=" flex flex-col lg:flex-row w-full min-h-screen">
       {/* Bagian Kiri */}
-      <form className="p-8 lg:p-16 lg:w-2/3 flex items-center justify-center bg-secret-cyan overflow-hidden flex-1">
+      <form onSubmit={handleLogin} className="p-8 lg:p-16 lg:w-2/3 flex items-center justify-center bg-secret-cyan overflow-hidden flex-1">
         <div className="w-full lg:w-2/3 text-black flex flex-col">
           <h1 className="font-bold text-3xl text-whit  lg:mb-12 text-left">
             Selamat Datang !
@@ -45,11 +90,13 @@ export default function LoginPage() {
               id="password"
               placeholder="Password"
               className="border-2 rounded-2xl w-full p-2 text-secret-text col-span-2 mt-4 shadow-2xl focus:shadow-none focus:outline-none"
+              pattern=".{8,}" 
+              title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
               required
             />
           </div>
 
-          <button type="submit" className="font-bold bg-secret-green text-white rounded-lg w-full p-2 shadow-2xl hover:shadow-none hover:scale-x-95 duration-300">
+          <button disabled={disabled} type="submit" className="font-bold bg-secret-green text-white rounded-lg w-full p-2 shadow-2xl hover:shadow-none hover:scale-x-95 duration-300">
 
             Masuk
           </button>
