82de30d62943b9101a3447ecf9e5e8130ad27a42
norplys <90627012+norplys@users.noreply.github.com>
2023-12-10 20:41:07 +0700

    Kode inayah (#22)
    * feat : adding homepage filter but still buggy
    
    * feat : finishing homepage filter
    
    * fix : fixing class button and my-class
    
    * fix : fixing home button
    
    * fix : fixing overflow typo
    
    * betulin 2
    
    * Filter feature (#18) (#20)
    
    * feat : adding homepage filter but still buggy
    
    * feat : finishing homepage filter
    
    * fix : fixing class button and my-class
    
    * fix : fixing home button
    
    * fix : fixing overflow typo
    
    * Filter feature (#18) (#21)
    
    * feat : adding homepage filter but still buggy
    
    * feat : finishing homepage filter
    
    * fix : fixing class button and my-class
    
    * fix : fixing home button
    
    * fix : fixing overflow typo
    
    * fix : fixing code
    
    * fix : fixing code
    
    * fix : fixing code
    
    * fix : fixing code
    
    ---------
    
    Co-authored-by: inayah <111202012998@mhs.dinus.ac.id>

---
+++ components/Admin/AdminCard.jsx
@@ -0,0 +1,15 @@
+import { LuUsers } from 'react-icons/lu';
+
+export const AdminCard = ({ statistic }) => {
+	return (
+		<div className={`flex items-center p-6 space-x-4 rounded-2xl ${statistic.color}`}>
+			<div className='relative flex-shrink-0 w-16 h-16 bg-white rounded-3xl'>
+				<LuUsers className='absolute w-8 h-8 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-secret-darkblue' />
+			</div>
+			<div className='flex flex-col'>
+				<h5 className='text-2xl text-white/80'>{statistic.count}</h5>
+				<span className='text-white'>{statistic.label}</span>
+			</div>
+		</div>
+	);
+};
---
+++ components/Admin/Sidebar.jsx
@@ -0,0 +1,80 @@
+import Link from 'next/link';
+import { BiX } from 'react-icons/bi';
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment, forwardRef } from 'react';
+import { IconButton } from '@/components/Admin/IconButton';
+import { AdminLogo } from '@/components/Admin/AdminLogo';
+
+export const Sidebar = ({ links, current, open, setOpen }) => {
+	return (
+		<>
+			<Transition.Root show={open} as={Fragment}>
+				<Dialog
+					as='div'
+					className='fixed inset-0 z-10 overflow-hidden bg-black bg-opacity-50 xl:hidden'
+					onClose={setOpen}>
+					<Transition.Child
+						as={Fragment}
+						enter='transition-opacity ease-linear duration-300'
+						enterFrom='opacity-0'
+						enterTo='opacity-100'
+						leave='transition-opacity ease-linear duration-300'
+						leaveFrom='opacity-100'
+						leaveTo='opacity-0'>
+						<Dialog.Overlay className='absolute inset-0' />
+					</Transition.Child>
+
+					<Dialog.Panel className='absolute inset-0 overflow-hidden' onClick={() => setOpen(false)}>
+						<Transition.Child
+							as={Fragment}
+							enter='transition ease-in-out duration-300 transform'
+							enterFrom='-translate-x-full'
+							enterTo='translate-x-0'
+							leave='transition ease-in-out duration-300 transform'
+							leaveFrom='translate-x-0'
+							leaveTo='-translate-x-full'>
+							<Content links={links} current={current} setOpen={setOpen} asDialog />
+						</Transition.Child>
+					</Dialog.Panel>
+				</Dialog>
+			</Transition.Root>
+
+			<Content links={links} current={current} asDialog={false} />
+		</>
+	);
+};
+
+const Content = forwardRef(({ links, current, asDialog, setOpen }, ref) => {
+	return (
+		<aside
+			ref={ref}
+			className={
+				`w-full max-w-[300px] bg-secret-darkblue transition-all duration-300 ease-out
+				${ asDialog ? 'absolute inset-y-0 left-0' : 'relative hidden xl:flex xl:flex-col'}`
+			}>
+			<div className='absolute bottom-0 left-0 m-6 xl:hidden'>
+				<IconButton icon={BiX} onClick={() => setOpen(false)} variants='secondary' />
+			</div>
+
+			<div className='flex items-center justify-center py-16'>
+				<AdminLogo />
+			</div>
+			{links?.map((link, index) => (
+				<Link
+					key={index}
+					href={link.href}
+					className={
+						`w-full block px-10 py-3 transition-all duration-200 ease-out'
+						${current === link.href ? 'bg-secret-background text-black' : 'text-white hover:bg-white/10'}`
+					}>
+					<p className='font-semibold'>{link.label}</p>
+				</Link>
+			))}
+			<button className='w-full px-10 py-3 text-white hover:bg-white/10 text-start'>
+				<p className='font-semibold'>Keluar</p>
+			</button>
+		</aside>
+	);
+});
+
+Content.displayName = 'Content';
