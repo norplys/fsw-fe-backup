5810f827d88b7187b5bc9abcf4cf87a47d12e2ba
daffabtara <btaradaffa@gmail.com>
2023-12-20 20:26:31 +0700

    feat: responsive

--- app/(checkout)/layout.jsx
+++ app/(checkout)/layout.jsx
@@ -1,10 +1,56 @@
+"use client"
 import Navbar from "@/components/Navbar"
+import { useState, useEffect } from "react"
+import { useRouter } from "next/navigation"
+import { useUsers } from "../context/usersContext"
+import Guard from "@/components/Guard"
+import { Toaster } from "react-hot-toast"
+
 
 export default function CheckOutLayout({children}) {
+  const router = useRouter()
+  const [loading, setLoading] = useState(true)
+  const {user, handleToken} = useUsers()
+
+  useEffect(()=>{
+    const token = localStorage.getItem("token")
+    ifToken(token)
+  }, [])
+
+  const ifToken = async (token) =>{
+    try{
+      if (token && !user){
+        await handleToken(token)
+        await mockLoading;
+        setLoading(false)
+      }else if(token && user){
+        await mockLoading
+        setLoading(false)
+      }else{
+        await mockLoading
+        router.push("/login")
+      }
+    }
+    catch(err){
+      await mockLoading
+      router.push("/login")
+    }
+  }
+  const mockLoading = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve(true)
+    }, 1000)
+  })
+
+  if(loading) return(<Guard/>)
+
+
+
     return (
       <section>
         <Navbar />
         {children}
+        <Toaster position="bottom-left"/>
       </section>
     )
   }
--- app/profile/layout.jsx
+++ app/profile/layout.jsx
@@ -1,6 +1,40 @@
+'use client'
 import Navbar from "@/components/Navbar"
+import { useEffect, useState } from "react"
+import { useUsers } from "../context/usersContext"
+import { useRouter } from "next/navigation"
+import Guard from "@/components/Guard"
 
 export default function CheckOutLayout({children}) {
+  useEffect(()=>{
+    const token = localStorage.getItem('token');
+    if(token){
+      ifToken(token);
+      setLoading(false);
+    }
+    else{
+      push('/login')
+    }
+
+      
+  }, [])
+    const {user, handleToken} = useUsers()
+    const {push} = useRouter()
+    const [loading, setLoading] = useState(true)
+    const ifToken = async (token)=>{
+      try{
+        if(token && !user){
+            await handleToken(token)
+        }
+      }
+      catch(err){
+        localStorage.removeItem('token')
+        push('/login')
+      }
+    }
+    if(loading){
+      return <Guard/>
+    }
     return (
       <section>
         <Navbar />
