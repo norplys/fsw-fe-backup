cdad3c24a1257727001dd6d529cbc6fe24830a8a
norplys <joywinata88@gmail.com>
2023-12-25 19:44:47 +0700

    fix : open class detail, fixing notification page

--- app/(course)/course/[id]/page.jsx
+++ app/(course)/course/[id]/page.jsx
@@ -33,7 +33,6 @@ const DetailKelas = () => {
   const [uuid, setUUID] = useState("")
   const { data: videoData, isLoading : videoLoading, error : videoError } = useVideoData(token, uuid);
   const queryClient = useQueryClient();
-  console.log(data);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -106,7 +105,7 @@ const DetailKelas = () => {
 
   const handleModal = () => {
     if (!token) {
-      push(`/login?redirect=/courses/${id}`);
+      push(`/login?redirect=/course/${id}`);
       return;
     }
     setIsOpen(true);
