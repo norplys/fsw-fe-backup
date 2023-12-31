6d612573eadb77d9590a75734fe2cf3e7c86f9b6
Daffa Btara <142818838+daffabtara@users.noreply.github.com>
2023-12-31 21:43:04 +0700

    Daffa btara (#56)
    * feat: Fix Responsive Navbar
    
    * feat: Fix Responsive LandingPage
    
    * feat: Fix Responsive LandingPage
    
    * feat: Fix Responsive MyCourses
    
    * feat: Fix Responsive Payment
    
    * feat: Fix Responsive MyCourses
    
    * feat: Fix Responsive Navbar
    
    * feat: Fix Responsive LandingPage lagi
    
    * feat: Fix Responsive LandingPage lagi
    
    * feat: kurangin shadow HomePageButton
    
    * feat: Fix Responsive Payment
    
    * feat: Fix Responsive LandingPage lagi..
    
    ---------
    
    Co-authored-by: norplys <joywinata88@gmail.com>

--- app/(course)/(courseProtector)/my-courses/page.jsx
+++ app/(course)/(courseProtector)/my-courses/page.jsx
@@ -180,14 +180,14 @@ const onChange = (e) => {
     levelFilterButton,
   ];
   return (
-    <div className=" bg-secret-grey2 px-[150px] pt-4 w-full font-montserrat min-h-screen">
+    <div className="bg-secret-grey2 md:px-[150px] pt-4 w-full font-montserrat min-h-screen">
       <section className="max-w-7xl m-auto">
-        <div className="flex justify-between ">
-          <h1 className="text-secret-text mb-10 font-bold text-[24px]">
+        <div className="flex justify-between md:flex-row flex-col p-2 md:p-0">
+          <h1 className="text-secret-text md:mb-10 mb-5 font-bold text-[24px]">
             Kelas Berjalan
           </h1>
 
-          <form className="flex h-fit border border-secret-darkblue pl-2 rounded-lg overflow-hidden shadow-xl" onSubmit={handleSubmit(handleSearch)}>
+          <form className="flex h-fit border border-secret-darkblue pl-2 rounded-lg overflow-hidden shadow-xl relative md:mb-0 mb-5" onSubmit={handleSubmit(handleSearch)}>
             <input
               {...register("search", {
                 onChange : (e) => onChange(e),
@@ -195,22 +195,22 @@ const onChange = (e) => {
               type="text"
               placeholder="Cari Kelas..."
               name="search"
-              className="text-black text-sm focus:outline-none"
+              className="text-black md:text-xl text-3xl focus:outline-none"
               id="search-class"
               value={searchValue}
             />
 
-            <button htmlFor="search-class" className="flex justify-center items-center p-2 bg-secret-darkblue hover:scale-110 duration-300" >
-              <BiSearchAlt className="text-secret-background text-xl"/>
+            <button htmlFor="search-class" className="flex md:static absolute right-1 top-[3px] justify-center items-center p-2 rounded-md bg-secret-darkblue hover:scale-110 duration-300" >
+            <BiSearchAlt className="text-secret-background text-sm"/>
             </button>
           </form>
         </div>
 
-        <div className="flex lg:flex-row gap-[88px] ">
+        <div className="flex lg:flex-row flex-col gap-[88px] ">
 
           <form
             className="bg-secret-background border shadow-md flex flex-col w-64 p-5
-            h-[580px] rounded-[16px] gap-5"
+            h-[580px] rounded-[16px] gap-5 mx-auto"
           >
             {isLoadingCategories ? 
               <div>Loading...</div>
@@ -246,7 +246,7 @@ const onChange = (e) => {
 
           <div className="container">
 
-            <div className="flex  gap-7 mb-5">
+            <div className="flex  md:gap-7 xl:gap-7 lg:gap-7 gap-2 mr-1 ml-1 mb-5">
               {ButtonData.map((item, index) => {
                 return (
                   <ClassButton
@@ -259,7 +259,7 @@ const onChange = (e) => {
                 );
               })}
             </div>
-            <div className="grid grid-cols-2 max-w-3xl flex-wrap flex-row content-start gap-6">
+            <div className="grid md:grid-cols-2 grid-flow-cols-1 max-w-3xl flex-wrap flex-row content-start gap-6">
               {isLoading ? 
                 array.map((item, index) => {
                   return <ClassCardLoading key={index} />;
--- app/page.jsx
+++ app/page.jsx
@@ -78,11 +78,11 @@ export default function Beranda () {
       <div className="font-montserrat min-h-screen">
         <div className="flex w-full">
           <div className="before:bg-gradient-to-t from-transparent to-[#2FB5BF] before:w-full before:h-full before:absolute relative col-span-12 flex-2 w-full">
-            <div className="bg-[url('/homeImage.svg')] bg-cover h-96 flex justify-center items-center">
-              <div className="z-10  flex items-center text-white font-bold text-5xl w-96 drop-shadow-[0_3px_1.2px_rgba(0,0,0,0.8)]">
+            <div className="bg-[url('/homeImage.svg')] bg-cover h-96 flex justify-center items-center flex-col md:flex-row">
+              <div className="z-10  flex items-center text-white font-bold text-5xl md:w-96  drop-shadow-[0_3px_1.2px_rgba(0,0,0,0.8)] flex-shrink-0 ml-5 md:ml-0">
                   Ready To Upgrade Your Skill ?
               </div>
-              <div className="flex gap-3">
+              <div className="flex gap-3 ml-5 md:ml-0 mt-5 md:mt-0">
               <Link className="text-white font-bold text-2xl z-10 border border-white h-min p-2 rounded-md animate-pulse hover:scale-105" href={"/courses"}>Mulai Sekarang</Link>
               <Link className="text-white font-bold text-2xl z-10 border border-white h-min p-2 rounded-md flex items-center hover:scale-105" href={"https://wa.me/6282284134328?text=Saya%20ingin%20bertanya%20perihal%20course%20di%20skillHUB"}> <CiHeadphones/> Tanya Kami</Link>
               </div>
@@ -101,7 +101,7 @@ export default function Beranda () {
               </Link>
             </div>
    
-            <div className="flex gap-5 pb-5">
+            <div className="flex gap-5 pb-5 overflow-x-auto md:overflow-hidden-x-auto lg:overflow-x-auto xl:overflow-hidden 2xl:overflow-hidden">
               {isLoadingCategories ? (
                 array.map((item, index) => {
                   return <CategoryLoading key={index} />;
@@ -111,12 +111,14 @@ export default function Beranda () {
               ) : (
                 dataCategories?.map((item, index) => {
                   return (
-                    <CardCategories
+                    <div key={index} className="flex-shrink-0 flex-grow w-[25%] xl:w-[10%]">
+                        <CardCategories
                       key={index}
                       name={item.name}
                       image={item.image}
                       categoryId={item.id}
                     />
+                    </div>
                   );
                 })
               )}
@@ -135,7 +137,7 @@ export default function Beranda () {
               </Link>
             </div>
 
-            <div className="flex gap-5  mb-5 justify-around">
+            <div className="flex gap-5  mb-5 justify-around overflow-x-auto p-1">
             {isLoadingCategories ? (
                 array.map((item, index) => {
                   return <ButtonLoading key={index} />;
@@ -152,7 +154,7 @@ export default function Beranda () {
             </div>
 
     
-            <div className="grid grid-cols-3 mx-auto  gap-5 items-center h-full">
+            <div className="grid grid-cols-1 md:grid-cols-3 mx-auto   gap-5 items-center h-full">
               {isLoadingCourses ? (
                 array.map((item, index) => {
                   return <ClassCardLoading key={index} />;
