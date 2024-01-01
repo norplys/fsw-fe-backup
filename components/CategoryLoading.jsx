736cc6f9ac233e734c6935dffb341e6b295e4057
norplys <90627012+norplys@users.noreply.github.com>
2024-01-01 16:40:42 +0700

    Api integration (#74)
    * feat : adding field array to admin form
    
    * fix : fixing scroll bar taking widt; protection bugg; admin form
    
    * feat : updating new feature
    
    * fix : fixing otp validation, adding payment
    
    * fix : fixing css
    
    * feat : adding mock payment-success
    
    * feat : updating new feature for login redirect and payment
    
    * fix :fixing conflict
    
    * feat : adding next button
    
    * feat : changing admin api, fixing course detail, adding admin search logic
    
    * feat : adding new feature
    
    * feat : new feature reset password
    
    * feat : adding freemodal premium modal component, video loading, fix : fixing profile course id, adding onboarding, adding reset password, fixing admin protection
    
    * fix : fixing auth bugg
    
    * delete page
    
    * feat: integrating onboarding api, update video api, payment history
    
    * fix : fixing admin protection
    
    * fix : open class detail, fixing notification page
    
    * fix : fixing profile
    
    * feat : adding preview image for admin dashboard
    
    * fix : fixing file preview
    
    * fix : fix reset password
    
    * Fix : fixing progress bar, and reset password
    
    * fix : sync be for classDetail
    
    * prep : preparing for profile api
    
    * prep : preparing my course filter and utils
    
    * feat : adding integration notification, user-password
    
    * fix : fixing otp input
    
    * feat : adding my course data ; fix : fixing search bar on courses bug
    
    * fix : course detail
    
    * fix : fixing premium button on class detail
    
    * feat : integrating notification
    
    * fix : fixing my courses filter, search bar on navbar and courses
    
    * fix : fixing admin page icl : navbar statistic payment status
    
    * fix : fixing next button
    
    * fix : fixing >> to 0 error
    
    * fix
    : fixing currency, and level
    feat : changing toast languange
    
    * feat : adding post course data & adding new validation feature
    
    * fix : fixing post data
    
    * fix : fixing course create and update
    
    * fix : fixing admin course design, course form and chapter module
    
    * fix : fixing form
    
    * fix : fixing course update
    
    * fix : fixing course create ; image optional
    
    * fix : fixing courses loading;  filter loading
    
    * fix : fixing responsive

--- components/CategoryLoading.jsx
+++ components/CategoryLoading.jsx
@@ -1,6 +1,6 @@
 export default function CategoryLoading() {
     return (
-      <div className="animate-pulse grid grid-rows-6 gap-3 w-96 h-40 flex-1 rounded-xl">
+      <div className="animate-pulse grid grid-rows-6 gap-3 min-w-96 min-h-40 flex-1 rounded-xl">
           <div className=" row-span-4 bg-secret-cyan rounded-3xl shadow-2xl "></div>
           <div className=" row-start-5 row-span-2 bg-secret-pink rounded-3xl shadow-2xl "></div>
       </div>
--- components/HomePage/CardCategories.jsx
+++ components/HomePage/CardCategories.jsx
@@ -6,12 +6,12 @@ export default function CardCategories ({name, image, categoryId}) {
     <>
         <Link className="flex-col w-full" href={`/courses?categoryId=${categoryId}`}>
             <Image
-            className='w-full object-cover hover:scale-105 ease-in-out duration-200'
+            className='w-full object-cover hover:scale-105 ease-in-out duration-200 md:w-56'
             src={image}
             width={140}
             height={100}
             alt={name}/>
-            <p className='text-md text-center text-secret-text font-semibold'>{name}</p>
+            <p className='text-center text-secret-text font-semibold text-sm md:text-lg'>{name}</p>
         </Link>
     </>
     );
