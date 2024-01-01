46d0b2b3af26b8dac78a692417e331d69132c101
norplys <joywinata88@gmail.com>
2024-01-02 00:12:26 +0700

    fix : fixing notification

--- app/profile/notification/page.jsx
+++ app/profile/notification/page.jsx
@@ -9,13 +9,13 @@ const Notification = () => {
   const { data, isLoading } = useNotification(token);
   return (
     <>
-      <div className="h-auto md:h-36 p-6 bg-secret-blue">
+      <div className="h-auto  p-3 md:p-6 bg-secret-blue min-h-screen">
         <Link
           href="/"
-          className="flex justify-start md:px-8 gap-6 mb-8 text-xl items-center hover:text-secret-darkblue"
+          className="flex justify-start md:px-8 gap-6 mb-8 text-sm md:text-xl items-center hover:text-secret-darkblue"
         >
           <FaArrowLeft />
-          <h1 className="font-bold">Kembali Ke Beranda</h1>
+          <h1 className="font-bold text-sm md:text-base">Kembali Ke Beranda</h1>
         </Link>
         <div className="flex justify-center items-center ">
           <div className="card bg-white w-full md:w-3/4 lg:w-3/6 xl:w-2/4 h-fit  rounded-xl border border-secret-pink">
